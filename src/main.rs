ad8db047faaa59ee6099a405eb48575b61554fbb
natejms <natejms@njms.ca>
2018-08-18 14:27:33 -0300

    Added `--quiet option` that suppresses non-error output.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,66 @@
-use std::{thread, time};
+use std::{thread, time, env, fs};
 use std::path::Path;
-use std::env;
-use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        3 => {
-            let path = Path::new(&args[1]);
-            let dest = Path::new(&args[2]);
+    if args.len() < 3 {
+        println!("Automatically move new files added to a directory to the destination.\n");
+        println!("Syntax: file-watch <source> <destination>\n");
 
-            println!("Starting loop.");
+        println!("Error: Missing arguments\n");
 
-            loop {
+        std::process::exit(1);
+    }
 
-                let file_paths = match fs::read_dir(&path) {
-                    Ok(paths) => paths,
-                    Err(e) => {
-                        println!("Failed to read source directoy:\n{:?}", e);
-                        thread::sleep(time::Duration::from_secs(5));
+    let path = Path::new(&args[1]);
+    let dest = Path::new(&args[2]);
 
-                        continue;
-                    }
-                };
+    let quiet = if args.contains(&"--quiet".to_string()) { true } else { false };
 
-                for file in file_paths {
-                    let f = match file {
-                        Ok(f) => f,
-                        Err(e) => {
-                            println!("Failed to read file: {:?}", e);
-                            continue;
-                        }
-                    };
+    if !quiet { println!("Starting loop.") }
 
-                    match fs::copy(&f.path(), &dest.join(&f.file_name())) {
-                        Ok(bytes) => println!("Copied {:?}: {} bytes", f.path(), bytes),
-                        Err(e) => {
-                            println!("Couldn't copy file {:?}: {:?}", f.path(), e);
-                            continue;
-                        }
-                    }
+    loop {
 
-                    match fs::remove_file(f.path()) {
-                        Ok(_) => println!("Removing copied file {:?}", f.path()),
-                        Err(e) => println!("Couldn't remove file {:?}: {:?}", f.path(), e)
-                    }
-                }
-
+        let file_paths = match fs::read_dir(&path) {
+            Ok(paths) => paths,
+            Err(e) => {
+                println!("Failed to read source directoy:\n{:?}", e);
                 thread::sleep(time::Duration::from_secs(5));
+
+                continue;
             }
-        },
-        _ => {
-            println!("Automatically move new files added to a directory to the specified destination.\n");
-            println!("Syntax: file-watch <source> <destination>");
+        };
+
+        for file in file_paths {
+            let f = match file {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("Failed to read file: {:?}", e);
+                    continue;
+                }
+            };
+
+            match fs::copy(&f.path(), &dest.join(&f.file_name())) {
+                Ok(bytes) => {
+                    if !quiet { println!("Copied {:?}: {} bytes", f.path(), bytes) }
+                },
+                Err(e) => {
+                    println!("Couldn't copy file {:?}: {:?}", f.path(), e);
+                    continue;
+                }
+            }
+
+            match fs::remove_file(f.path()) {
+                Ok(_) => {
+                    if !quiet { println!("Removing copied file {:?}", f.path()) }
+                },
+                Err(e) => {
+                    println!("Couldn't remove file {:?}: {:?}", f.path(), e)
+                }
+            }
         }
+
+        thread::sleep(time::Duration::from_secs(5));
     }
 }
